381dd76f3ce7b14c6a4ebcce69772553b1a9d609
unknown <iammukeshious@gmail.com>
2025-07-01 13:05:52 +0545

    project page completed

--- src/Pages/Project.tsx
+++ src/Pages/Project.tsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import About from './About'
 
 const Project = () => {
   return (
-    <div className=''>
-      
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-2 justify-items-center">
+      <div className="card bg-base-100 w-96 shadow-sm mt-1.5 mb-1.5">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Ecommerce</h2>
+          <p>Uisng php and mysql and css dynamic project for ecommerce platfoem </p>
+          <div className="card-actions justify-end">
+          </div>
+        </div>
+      </div>
+
+      <div className="card bg-base-100 w-96 shadow-sm mt-1.5 mb-1.5">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Ecommerce</h2>
+          <p>Uisng php and mysql and css dynamic project for ecommerce platfoem </p>
+          <div className="card-actions justify-end">
+          </div>
+        </div>
+      </div>
+
+      <div className="card bg-base-100 w-96 shadow-sm mt-1.5 mb-1.5">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Ecommerce</h2>
+          <p>Uisng php and mysql and css dynamic project for ecommerce platfoem </p>
+          <div className="card-actions justify-end">
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
